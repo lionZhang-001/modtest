771b2abf7d05844b586ef9e50e90097b131e3332
zyy <837409055@qq.com>
2019-09-24 19:19:40 +0800

    first commit

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/slamidiot/modtest
+
+go 1.13
---
+++ modtest.go
@@ -0,0 +1,9 @@
+ 
+package modtest
+
+import "fmt"
+
+// Hi 返回一个友好的问候
+func Hi(name string) string {
+   return fmt.Sprintf("Hi, %s", name)
+}
