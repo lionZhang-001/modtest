2f9f3f15b143657c516ed3d6614a8395571e2443
zyy <837409055@qq.com>
2019-09-24 19:50:20 +0800

    Emphasize our friendliness

--- modtest.go
+++ modtest.go
@@ -5,5 +5,6 @@ import "fmt"
 
 // Hi 返回一个友好的问候
 func Hi(name string) string {
-   return fmt.Sprintf("Hi, %s", name)
+   //return fmt.Sprintf("Hi, %s", name)
+    return fmt.Sprintf("Hi, %s!", name)
 }
